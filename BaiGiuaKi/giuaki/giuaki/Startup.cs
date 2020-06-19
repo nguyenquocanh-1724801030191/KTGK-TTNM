0852d79edc822e2dbe99918d64301dfd37f633ec
Anh Quoc <1999quocanh233@gmail.com>
2020-06-19 22:32:20 +0700

    first commit

---
+++ BaiGiuaKi/giuaki/giuaki/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(giuaki.Startup))]
+namespace giuaki
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
